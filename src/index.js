9fd248d5a55b3986ba348a162d382c7ed8ea40bb
Andrey <steamprofile2020@gmail.com>
2021-09-22 21:20:14 +0300

    firebase 2

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { HeaderComponent } from './components/header.component'
+import { NavigationComponent } from './components/navigation.component'
+import { PostsComponent } from './components/posts.component'
+import { CreateComponent } from './components/create.component'
+import { FavoriteComponent } from './components/favorite.component'
+
+new HeaderComponent('header')
+
+const navigation = new NavigationComponent('navigation')
+
+const posts = new PostsComponent('posts')
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
+
+navigation.registerTabs([
+    { name: 'posts', component: posts },
+    { name: 'create', component: create },
+    { name: 'favorite', component: favorite }
+])
