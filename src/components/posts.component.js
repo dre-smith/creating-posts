970b007cbb0b09f92ad1e95858e02be5be1ee6a5
Andrey <steamprofile2020@gmail.com>
2021-09-23 19:17:38 +0300

    html delete

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -10,6 +10,26 @@ export class PostsComponent extends Component {
     async onShow() {
         const fbData = await apiService.fetchPosts()
         const posts = TransformService.fbObjectToArray(fbData)
-        console.log(posts)
+        const html = posts.map(post => renderPost(post))
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
     }
+}
+
+function renderPost(post) {
+    return `
+        <div class="panel">
+            <div class="panel-head">
+                <p class="panel-title">Название поста</p>
+                <ul class="tags">
+                <li class="tag tag-blue tag-rounded">Новость</li>
+                </ul>
+            </div>
+            <div class="panel-body">
+                <p class="multi-line">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="panel-footer w-panel-footer">
+                <small>12.12.12</small>
+            </div>
+        </div>
+    `
 }
