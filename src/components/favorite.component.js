c0ac918dcf02ef5a396504a65c82bb076b58e768
Andrey <steamprofile2020@gmail.com>
2021-09-27 22:10:54 +0300

    list output

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -1,9 +1,12 @@
 import { Component } from '../core/component'
 import { apiService } from '../services/api.service'
+import { renderPost } from '../templates/post.template'
 
 export class FavoriteComponent extends Component {
-    constructor(id) {
+    constructor(id, options) {
         super(id)
+
+        this.loader = options.loader
     }
 
     init() {
@@ -15,6 +18,10 @@ export class FavoriteComponent extends Component {
         const html = renderList(favorites)
         this.$el.insertAdjacentHTML('afterbegin', html)
     }
+
+    onHide() {
+        this.$el.innerHTML = ''
+    }
 }
 
 async function linkClickHandler(event) {
@@ -22,8 +29,11 @@ async function linkClickHandler(event) {
 
     if (event.target.classList.contains('js-link')) {
         const postId = event.target.textContent
-
+        this.$el.innerHTML = ''
+        this.loader.show()
         const post = await apiService.fetchPostById(postId)
+        this.loader.hide()
+        this.$el.insertAdjacentHTML('afterbegin', renderPost(post, { withButton: false }))
     }
 }
 
