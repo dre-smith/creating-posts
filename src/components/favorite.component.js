67930bf3c4ad817397ab182b22e2dfd4987ff2f7
Andrey <steamprofile2020@gmail.com>
2021-09-27 20:01:30 +0300

    list output

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -4,4 +4,30 @@ export class FavoriteComponent extends Component {
     constructor(id) {
         super(id)
     }
+
+    init() {
+        this.$el.addEventListener('click', linkClickHandler.bind(this))
+    }
+
+    onShow() {
+        const favorites = JSON.parse(localStorage.getItem('favorites'))
+        const html = renderList(favorites)
+        this.$el.insertAdjacentHTML('afterbegin', html)
+    }
+}
+
+function linkClickHandler(event) {
+    event.preventDefault()
+}
+
+function renderList(list = []) {
+    if (list.length) {
+        return `
+        <ul>
+            ${list.map(i => `<li><a href="#">${i}</a></li>`).join(' ')}
+        </ul>
+        `
+    }
+
+    return `<p class="center">Вы пока ничего не добавили</p>`
 }
